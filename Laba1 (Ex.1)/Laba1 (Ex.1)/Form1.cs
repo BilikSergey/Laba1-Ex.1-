1091f0911b99c872bc030123f9ace6b1df1a9c39
BilikSergey <58261565+BilikSergey@users.noreply.github.com>
2021-03-13 17:43:47 +0200

    Add files via upload

---
+++ Laba1 (Ex.1)/Laba1 (Ex.1)/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Laba1__Ex._1_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Hide();
+            Form2 newForm = new Form2();
+            newForm.Show();
+           
+        }
+    }
+}
