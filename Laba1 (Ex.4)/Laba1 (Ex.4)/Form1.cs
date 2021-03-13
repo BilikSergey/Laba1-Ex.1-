5037931d76961b2662c88af50c32e8bde700b8c1
BilikSergey <58261565+BilikSergey@users.noreply.github.com>
2021-03-13 17:52:22 +0200

    Add files via upload

---
+++ Laba1 (Ex.4)/Laba1 (Ex.4)/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Laba1__Ex._4_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        int lCount = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FontFamily fontFamily = new FontFamily("Arial");
+            Font font = new Font(
+               fontFamily,
+               10,
+               FontStyle.Bold,
+               GraphicsUnit.Pixel);
+
+            lCount++;            
+            Label lbl = new Label();
+            lbl.Text = "Label" + lCount.ToString();
+            lbl.Location = new Point(10, 10 + 30 * (lCount - 1));
+            this.Controls.Add(lbl);
+            lbl.Font = font;
+        }
+    }
+}
