8cc1d66aa789405b85a8bf80e91bac7e76a9586d
BilikSergey <58261565+BilikSergey@users.noreply.github.com>
2021-03-29 21:02:42 +0300

    Add files via upload

---
+++ C# Laba 2/C# Laba 2(Ex.2)/Laba 2/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        delegate void Check();
+        Check ch1;
+        Check ch2;
+        Check ch3;
+        public Form1()
+        {
+            InitializeComponent();           
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            { ch1 = Bt1; ch1(); }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            { ch2 = Bt2; ch2(); }       
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            { ch3 = Bt3; ch3(); }
+        }       
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Я супермегакнопка, i цього мене не позбавиш!");
+            if (checkBox1.Checked) { ch1 = Bt1; ch1(); }
+            if (checkBox2.Checked) { ch2 = Bt2; ch2(); }
+            if (checkBox3.Checked) { ch3 = Bt3; ch3(); }
+        }
+        public void Bt1()
+        {
+            if (this.Opacity != 0.75 && this.Opacity == 1) this.Opacity = 0.75;
+            else this.Opacity = 1;
+        }
+        private void Bt2()
+        {
+            if (BackColor == Color.Red && BackColor != Color.Gray) this.BackColor = System.Drawing.Color.Gray;
+            else this.BackColor = System.Drawing.Color.Red;
+        }
+        private void Bt3()
+        {
+            MessageBox.Show("Hello World!");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }       
+    }
+}
---
+++ C# Laba 2/C# Laba2(Ex.1)/Laba2(Ex.1)/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public delegate double Decision(double number);
+   class Program
+    {
+
+       private static double Sqrt(double number) { return Math.Sqrt(Math.Abs(number)); }
+
+       private static double Myltiply(double number) { return Math.Pow(number, 3); }
+
+       private static double Plus(double number) { return number + 3.5; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вводьте рядки послiдовно один за одним.\n" +
+                "Поки вони матимуть вигляд 0 x чи 1 x чи 2 x\n" +
+                "(тобто, цифра вiд 0 до 2, а пiсля неї запис конкретного дiйсного числа),\n" +
+                "програма обчислюватиме одну з трьох функцiй\n " +
+                "\t 0 -- sqrt(abs(x))\n" +
+                "\t 1 -- x^3 (?накше кажучи, x*x*x)\n" +
+                "\t 2 -- x + 3,5");
+            Decision[] op = new Decision[]
+            {Sqrt, Myltiply, Plus};
+
+            bool start = true;
+
+            while (start)
+            {
+                try
+                {
+                    string str = Console.ReadLine();
+                    string[] s = str.Split(' ');
+                    Console.WriteLine(op[int.Parse(s[0])](double.Parse(s[1])));
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    start = false;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
